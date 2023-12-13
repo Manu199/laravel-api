db80363bd9ef3e5acb60c8384ba2c13cacc904eb
Manu199 <manu199gh@gmail.com>
2023-12-13 17:22:39 +0100

    [add] newContact

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class LeadController extends Controller
 {
-    //
+    public function store()
+    {
+    }
 }
---
+++ resources/views/mail/new_contact.blade.php
@@ -0,0 +1,9 @@
+<>
+    Hai ricevuto un nuovo messaggio: <br>
+
+    Nome: {{ $lead->name }} <br>
+    Email: {{ $lead->email }} <br>
+    Messaggio: <br>
+    {{ $lead->message }}
+
+    </p>
